ddc6ed10b0eb6a5206b7ba1d7a5ee79c9a6d1bc2
Harsh Wardhan <hwardhan337@gmail.com>
2020-05-27 09:19:56 +0530

    Tilting buttons added

--- lib/tilt.dart
+++ lib/tilt.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'config.dart';
 import 'package:sensors/sensors.dart';
+import 'package:flutter/services.dart';
 void tilt(){
   final subscription = accelerometerEvents.listen((AccelerometerEvent event) {
     gcurr = event.y;
@@ -15,12 +16,16 @@ double min(double a,double b){
     return a;
   }
 }
+void _send(char) {
+  print("Sending " + char);
+  sock.write('wasd'+'&'+char+'%');
+}
 void tsend(){
   if(gcurr>0.4){
-    String s = min(gcurr/7,1).toString();
+    String s = min(gcurr/10,1).toString();
     sock.write("tilt&+&"+s+'%');
   }else if(gcurr<-0.4){
-    String s =  min((-1*gcurr)/7,1).toString();
+    String s =  min((-1*gcurr)/10,1).toString();
     sock.write("tilt&-&"+s+'%');
   }else{
     sock.write("tilt&+&"+'0'+'%');
@@ -31,6 +36,23 @@ class Gyro extends StatefulWidget{
   _GyroState createState() => _GyroState();
 }
 class _GyroState extends State<Gyro> {
+  @override
+  void initState(){
+    super.initState();
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+    ]);
+  }
+  @override
+  dispose(){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context){
     if(!ovisit){
@@ -46,19 +68,63 @@ class _GyroState extends State<Gyro> {
       appBar: AppBar(
         title: Text("Tilt to Control"),
       ),
-      body: Center( 
-        child : Checkbox(
-        value: tiltcontrol,
-        onChanged: (bool value) {
-          if(!value){
-            sock.write("tilt&0%");
-          }
-          setState(() {
-            tiltcontrol=value;
-          });
-        },
-        )
-      )
+      body:Padding(padding: EdgeInsets.all(2.0),
+        child: GridView.count(crossAxisCount: 3,
+        children: <Widget>[
+        Center(
+          child:GestureDetector(
+            onTapDown: (_) {
+              _send('down&w');
+            },
+            onTapUp: (_) {
+               _send('up&w');
+            },
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(80.0),
+              child: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+                size: 45.0,
+              ),
+            ),
+          ),
+        ),
+          Center( 
+            child : Checkbox(
+            value: tiltcontrol,
+            onChanged: (bool value) {
+              if(!value){
+                sock.write("tilt&0%");
+              }
+              setState((){
+                tiltcontrol=value;
+              });
+            },
+          )
+        ),
+        Center(
+          child:GestureDetector(
+            onTapDown: (_) {
+              _send('down&d');
+            },
+            onTapUp: (_) {
+               _send('up&d');
+            },
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(80.0),
+              child: Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+                size: 45.0,
+              ),
+            ),
+          ),
+        ),
+      ]
+    )
+    )
     );
   }
 }
